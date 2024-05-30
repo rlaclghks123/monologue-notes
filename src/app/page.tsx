2ed8c89bbf6e243c4bef26c291aecbcce7def39c
rlaclghks123 <rlaclghks123@naver.com>
2024-05-30 20:26:09 +0900

    refactor: 홈페이지 반복되는 요소의 내용 객체로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 import BookIcon from '../../public/svgs/bookIcon.svg';
 
+const DATA = [
+  {
+    id: 1,
+    content: '1단계 : 목표설정 → 독서를 시작하기 전에 배우고 싶은 부분을 적어보세요.',
+  },
+  {
+    id: 2,
+    content: '2단계: 내용 확인 → 독서를 진행하면서 저자가 이 의문점에 어떻게 답하는지 확인하세요.',
+  },
+  {
+    id: 3,
+    content: '3단계: 정리 → 기록과 적용 독서 후, 배운 내용을 기록하고 실제로 적용해보세요.',
+  },
+];
+
 export default function Home() {
   return (
     <main className="h-screen bg-[url('../../public/images/mainBgImg.jpg')] bg-no-repeat bg-cover bg-center">
@@ -13,14 +28,13 @@ export default function Home() {
             독백노트를 통해 세 가지 단계로 기록하여 독서의 시간을 낭비하지 않고, 얻은 지식을
             오래도록 기억해보세요
           </p>
+
           <section className="pt-60 flex flex-col items-center text-light-beige">
-            <p className="pb-5">
-              1단계 : 목표설정 → 독서를 시작하기 전에 배우고 싶은 부분을 적어보세요.
-            </p>
-            <p className="pb-5">
-              2단계: 내용 확인 → 독서를 진행하면서 저자가 이 의문점에 어떻게 답하는지 확인하세요.
-            </p>
-            <p>3단계: 정리 → 기록과 적용 독서 후, 배운 내용을 기록하고 실제로 적용해보세요.</p>
+            {DATA.map(({ id, content }) => (
+              <p key={id} className="pb-5">
+                {content}
+              </p>
+            ))}
           </section>
         </div>
 
