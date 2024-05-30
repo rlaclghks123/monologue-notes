a92ef5779ce25c9b132529f8fc0daf00afb78d9d
rlaclghks123 <rlaclghks123@naver.com>
2024-05-30 20:21:56 +0900

    feat: home페이지 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
+
 import '../styles/reset.css';
+import '../styles/global.css';
 
 export const metadata: Metadata = {
-  title: 'monologue-notes',
-  description: 'This site is that helps you read books effectively',
+  title: '독백노트',
+  description:
+    '독서를 통해 배우고 싶은 부분을 미리 적어보고, 저자가 전달하는 내용은 무엇인지, 내가 원하던 의문점을 저자가 해결해주었는지, 그것이 맞다면 내것으로 만들기 위해 기록해보는 서비스입니다.',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import BookIcon from '../../public/svgs/bookIcon.svg';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="h-screen bg-[url('../../public/images/mainBgImg.jpg')] bg-no-repeat bg-cover bg-center">
+      <section className="p-20 h-full flex flex-col items-center">
+        <h1 className="text-6xl text-light-beige">독백노트</h1>
+        <div className="text-light-beige p-20 text-2xl text-center">
+          <p>독서를 하고 느낀 점을 기록해도 다시 보지 않아 기억에 남지 않던 경험이 있으신가요?</p>
+          <p>
+            독백노트를 통해 세 가지 단계로 기록하여 독서의 시간을 낭비하지 않고, 얻은 지식을
+            오래도록 기억해보세요
+          </p>
+          <section className="pt-60 flex flex-col items-center text-light-beige">
+            <p className="pb-5">
+              1단계 : 목표설정 → 독서를 시작하기 전에 배우고 싶은 부분을 적어보세요.
+            </p>
+            <p className="pb-5">
+              2단계: 내용 확인 → 독서를 진행하면서 저자가 이 의문점에 어떻게 답하는지 확인하세요.
+            </p>
+            <p>3단계: 정리 → 기록과 적용 독서 후, 배운 내용을 기록하고 실제로 적용해보세요.</p>
+          </section>
+        </div>
+
+        <Link href="/category" className="bg-transparent">
+          <Image src={BookIcon} alt="book icon" />
+          <div className="text-light-beige text-center">시작하기</div>
+        </Link>
+      </section>
+    </main>
+  );
 }
