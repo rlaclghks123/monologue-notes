cc28427424a3a192d1134e0929ecc4f492ab9c76
rlaclghks123 <rlaclghks123@naver.com>
2024-06-13 18:53:55 +0900

    refactor: Homepage 수정

---
+++ src/app/(HomeBanner).tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+const DATA = [
+  {
+    id: 1,
+    content: '첫째 - 독서를 시작하기 전에 배우고 싶은 부분을 적어보세요.',
+  },
+  {
+    id: 2,
+    content: '둘째 - 배우고 싶은 부분을 저자가 어떻게 답하는지 적어보세요.',
+  },
+  {
+    id: 3,
+    content: '셋째 - 배울점을 기록하고 실제로 적용해보세요.',
+  },
+];
+
+export default function HomeBanner() {
+  return (
+    <section className="mt-5 box-border flex h-52 w-full justify-between rounded-lg bg-peach-fuzz">
+      <div className="flex h-full w-1/2 flex-col items-center justify-center">
+        <Image src="/images/bookBoy.png" width="120" height="120" alt="책" priority />
+        <p className="text-base">독백노트를 통해 3단계로 기록하여 지식을 오래도록 기억해보세요</p>
+      </div>
+
+      <div className=" flex h-full w-1/2 items-center justify-center text-base">
+        <div className="flex h-full flex-col justify-evenly">
+          {DATA.map(({ id, content }) => (
+            <p key={id}>{content}</p>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,9 @@
-import Image from 'next/image';
+import HomeBanner from './(HomeBanner)';
 
-const DATA = [
-  {
-    id: 1,
-    content: '1. 독서를 시작하기 전에 배우고 싶은 부분을 적어보세요.',
-  },
-  {
-    id: 2,
-    content: '2. 배우고 싶은 부분을 저자가 어떻게 답하는지 적어보세요.',
-  },
-  {
-    id: 3,
-    content: '3. 배울점을 기록하고 실제로 적용해보세요.',
-  },
-];
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="w-ful h-full">
-      <section className="bg-peach-fuzz mt-5 box-border flex h-52 w-full justify-between rounded-lg">
-        <div className="flex h-full w-1/2 flex-col items-center justify-center">
-          <Image src="/images/book.png" width="200" height="200" alt="책" />
-          <p className="-mt-5 text-base">
-            독백노트를 통해 3단계로 기록하여 지식을 오래도록 기억해보세요
-          </p>
-        </div>
-
-        <div className=" flex h-full w-1/2 items-center justify-center text-base">
-          <div className="flex h-full flex-col justify-evenly">
-            {DATA.map(({ id, content }) => (
-              <p key={id}>{content}</p>
-            ))}
-          </div>
-        </div>
-      </section>
+      <HomeBanner />
     </div>
   );
 }
