8d424391391be60d346986badb136c2a244b661a
rlaclghks123 <rlaclghks123@naver.com>
2024-06-26 14:39:27 +0900

    refactor: Carousel link href 변수로 추출

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -13,19 +13,18 @@ interface Props {
 }
 
 export default function Carousel({ size, curPage }: Props) {
+  const totalPages = 5;
+  const prevPage = curPage === 1 ? totalPages : curPage - 1;
+  const nextPage = curPage === totalPages ? 1 : curPage + 1;
+
   return (
     <>
       <div className="absolute top-20 flex justify-center ">
-        <Link
-          href={`/?page=${curPage - 1 === 0 ? 5 : curPage - 1}`}
-          className="absolute left-[-24px] z-20 h-5 w-5"
-        >
+        <Link href={`/?page=${prevPage}`} className="absolute left-[-24px] z-20 h-5 w-5">
           <Image src={Left} alt="left" />
         </Link>
-        <Link
-          href={`/?page=${curPage + 1 === 6 ? 1 : curPage + 1}`}
-          className="absolute left-[1030px] z-20 h-5 w-5"
-        >
+
+        <Link href={`/?page=${nextPage}`} className="absolute left-[1030px] z-20 h-5 w-5">
           <Image src={Right} alt="right" />
         </Link>
       </div>
