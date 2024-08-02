96d9545a7ecbb8e21f2ef9f3bfdf862b39b3376d
rlaclghks123 <rlaclghks123@naver.com>
2024-08-02 11:22:07 +0900

    refactor: Button 컴포넌트 secondary 옵션 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,14 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary';
-  text: string;
+  text: ReactNode;
 }
 
 export default function Button({ variant = 'primary', text, ...props }: Props) {
   const CLASS_NAME = {
     primary: 'h-full w-full bg-white pl-3 hover:bg-gray-100',
-    secondary: '',
+    secondary: 'hover:bg-gray-300 p-2 rounded-full ml-[-10px] w-9 h-9',
   };
 
   return (
