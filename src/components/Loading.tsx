e653a3e7ceb4d24de1ba026f4a1535f8473bf28d
rlaclghks123 <rlaclghks123@naver.com>
2024-08-07 17:22:19 +0900

    refactor: Loading UI 200ms 지연

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from 'react';
 import Spinner from '../../public/svgs/spinner.svg';
 
 export default function Loading() {
+  const [showLoader, setShowLoader] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoader(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!showLoader) {
+    return null;
+  }
+
   return (
     <div className="flex h-36 w-full items-center justify-center">
       <div className="h-10 w-10 animate-spin">
-        <Spinner alt="Loading icon" className=" fill-peach-fuzz" />
+        <Spinner alt="Loading icon" className="fill-peach-fuzz" />
       </div>
     </div>
   );
