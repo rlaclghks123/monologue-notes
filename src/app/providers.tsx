777b6b3dc1121d3ee7f204a5d4f0c553615e31fd
rlaclghks123 <rlaclghks123@naver.com>
2024-08-07 10:25:56 +0900

    chore: redux-toolkit 설치 및 설정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,14 @@
+'use client';
+
+import { Provider } from 'react-redux';
 import ClientSideQueryClientProvider from '@/global/ClientSideQueryClientProvider';
 
+import store from '../store';
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ClientSideQueryClientProvider>{children}</ClientSideQueryClientProvider>;
+  return (
+    <ClientSideQueryClientProvider>
+      <Provider store={store}>{children}</Provider>
+    </ClientSideQueryClientProvider>
+  );
 }
