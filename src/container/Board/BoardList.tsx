c7bb2a3e93f687b20a2b4187800f4a80b1fa8a08
rlaclghks123 <rlaclghks123@naver.com>
2024-08-08 11:23:47 +0900

    style: BoardLit 비율 수정

--- src/container/Board/BoardList.tsx
+++ src/container/Board/BoardList.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function BoardList({ posts }: Props) {
   return (
     <div className=" h-full overflow-scroll rounded-lg bg-white">
-      <ol className="grid gap-2 xs:grid-cols-1 xs:gap-4 sm:grid-cols-2 xl:grid-cols-3 xl:grid-rows-3-equal">
+      <ol className="grid h-full gap-2 p-2 xs:grid-cols-1 xs:gap-4 sm:grid-cols-2 xl:grid-cols-3 xl:grid-rows-3-equal">
         {posts.length === 0 &&
           Array.from({ length: 9 }, (_, idx) => <BoardCardSkeleton key={idx} />)}
 
