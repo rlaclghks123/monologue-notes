e82591d0431594b49a1ede857baa2d18a393ea50
rlaclghks123 <rlaclghks123@naver.com>
2024-08-08 11:31:58 +0900

    feat: 로그인/로그아웃시 UI 추가

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { loginHandler } from '@/service/user';
+import Spinner from '../../../public/svgs/spinner.svg';
 
 function Page() {
   loginHandler();
 
-  return <div>잠시만 기다려주세요...</div>;
+  return (
+    <div className="flex h-40 w-full items-center justify-center gap-6">
+      <p className="font-semibold text-peach-fuzz">잠시만 기다려주세요</p>
+      <div className="h-8 w-8 animate-spin">
+        <Spinner alt="Loading icon" className="fill-peach-fuzz" />
+      </div>
+    </div>
+  );
 }
 
 export default Page;
