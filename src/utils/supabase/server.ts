77784576ef926664f4aee21bd050b01a54fdbc95
rlaclghks123 <rlaclghks123@naver.com>
2024-08-11 14:44:06 +0900

    fix: server.ts 파일 변경

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,28 +1,31 @@
-'use server';
-
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-import { Database } from '@/types/supabase';
-
-const creatServer = async () => {
+export default function creatServer() {
   const cookieStore = cookies();
 
-  return createServerClient<Database>(
+  // Create a server's supabase client with newly configured cookie,
+  // which could be used to maintain user's session
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (key: string) => cookieStore.get(key)?.value,
-        set: (key: string, value: string, options: CookieOptions) => {
-          cookieStore.set(key, value, options);
+        getAll() {
+          return cookieStore.getAll();
         },
-        remove: (key: string, options: CookieOptions) => {
-          cookieStore.set(key, options);
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     },
   );
-};
-
-export default creatServer;
+}
