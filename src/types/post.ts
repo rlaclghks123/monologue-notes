2e8433e42c482159244404a0e32f9eb4449b5eab
rlaclghks123 <rlaclghks123@naver.com>
2024-08-11 13:39:09 +0900

    refactor: 사용하지 않는 코드 제거

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,28 +1,4 @@
-import { SelectedBook } from './book';
 import { Database } from './supabase';
-
-export type PostFormDataType = Omit<SelectedBook, 'subInfo'> & {
-  id?: number;
-  user_id?: string | null;
-  item_page: number;
-  nickname?: string;
-  avatar_url?: string;
-  before_read: string;
-  writer_say: string;
-  after_read: string;
-};
-
-export type GetPosts = PostFormDataType & {
-  created_at: string;
-};
-
-export interface PostAndCountData {
-  data?: {
-    data: GetPosts[] | null;
-    count: number;
-  };
-  isLoading: boolean;
-}
 
 export type ReadPost = Database['public']['Tables']['post']['Row'];
 export type CreatePost = Database['public']['Tables']['post']['Insert'];
