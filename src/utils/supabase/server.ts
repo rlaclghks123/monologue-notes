fba93ae91acac2f749f7cdaa66c3c6a9e5778005
rlaclghks123 <rlaclghks123@naver.com>
2024-08-11 11:35:08 +0900

    fix: supabase type 추가 및 middleware,client,server 클라이언트 수정

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,21 +1,23 @@
+'use server';
+
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-const creatServer = async (rsc = false) => {
+import { Database } from '@/types/supabase';
+
+const creatServer = async () => {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get: (key: string) => cookieStore.get(key)?.value,
         set: (key: string, value: string, options: CookieOptions) => {
-          if (rsc) return;
           cookieStore.set(key, value, options);
         },
         remove: (key: string, options: CookieOptions) => {
-          if (rsc) return;
           cookieStore.set(key, options);
         },
       },
