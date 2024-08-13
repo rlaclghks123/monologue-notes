00439af14a409857cdead677721ebbd09addcfe5
rlaclghks123 <rlaclghks123@naver.com>
2024-08-13 14:17:07 +0900

    feat: Board Page 스켈레톤 UI 적용

---
+++ src/app/board/[page]/loading.tsx
@@ -0,0 +1,22 @@
+import BoardCardSkeleton from '@/container/Board/BoardCardSkeleton';
+
+export default function loading() {
+  return (
+    <div className="h-screen-without-nav pb-20 pt-5">
+      <div className="mb-2 flex h-8 justify-end" />
+      <div className=" h-full overflow-scroll rounded-lg bg-white">
+        <ol className="grid h-full gap-2 p-2 xs:grid-cols-1 xs:gap-4 sm:grid-cols-2 xl:grid-cols-3 xl:grid-rows-3-equal">
+          {Array.from({ length: 9 }, (_, idx) => (
+            <BoardCardSkeleton key={idx} />
+          ))}
+        </ol>
+      </div>
+
+      <div className="mt-2 w-full">
+        <div className="flex items-center justify-center gap-2">
+          <div className="h-5 w-20 rounded-md bg-gray-200" />
+        </div>
+      </div>
+    </div>
+  );
+}
