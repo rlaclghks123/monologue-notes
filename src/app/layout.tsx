d483c7dbc38e59ef162297d8a12dc8a6ec9aaf27
rlaclghks123 <rlaclghks123@naver.com>
2024-08-13 15:00:19 +0900

    feat: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
   title: '독백노트',
   description:
     '독서를 통해 배우고 싶은 부분을 미리 적어보고, 저자가 전달하는 내용은 무엇인지, 내가 원하던 의문점을 저자가 해결해주었는지, 그것이 맞다면 내것으로 만들기 위해 기록해보는 서비스입니다.',
+  icons: {
+    icon: '/bookBoy.png',
+  },
 };
 
 export default function RootLayout({
