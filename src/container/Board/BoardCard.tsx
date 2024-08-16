5ba74aad4db49af86b5c4aeddbec0194bdaa8d30
rlaclghks123 <rlaclghks123@naver.com>
2024-08-17 01:38:14 +0900

    style: CardView 스타일 변경

--- src/container/Board/BoardCard.tsx
+++ src/container/Board/BoardCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 export default function BoardCard({ post }: Props) {
   return (
-    <li className="w-full cursor-pointer flex-col truncate rounded-lg border border-black p-2 hover:border hover:border-solid hover:border-gray-300 xs:h-24 sm:h-32 xl:h-40">
+    <li className="w-full transform cursor-pointer flex-col truncate rounded-xl border border-black bg-white p-2 shadow-md transition duration-300 hover:scale-105 xs:h-24 sm:h-32 xl:h-40">
       <Link href={`/detail/${post.id}`} className="flex h-full items-center">
         <Image
           src={post.cover || NoImg}
@@ -19,7 +19,7 @@ export default function BoardCard({ post }: Props) {
           priority
           className="h-full rounded-xl bg-gray-200 xs:min-w-16 xs:max-w-16 sm:min-w-20 sm:max-w-20 xl:min-w-24 xl:max-w-24"
         />
-        <div className="ml-2 flex h-full w-full flex-col justify-evenly truncate text-xs">
+        <div className="ml-4 flex h-full w-full flex-col justify-evenly truncate text-xs">
           <p>{`제목 : ${post.title}`}</p>
           <p>{`출판사 : ${post.publisher}`}</p>
           <p>{`독백자 : ${post.nickname ?? '익명'}`}</p>
--- src/container/Board/BoardList.tsx
+++ src/container/Board/BoardList.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function BoardList({ posts }: Props) {
   return (
-    <section className=" h-full overflow-scroll rounded-lg bg-white">
+    <section className=" h-full overflow-scroll rounded-lg">
       <ul className="grid h-full gap-2 p-2 xs:grid-cols-1 xs:gap-4 sm:grid-cols-2 xl:grid-cols-3 xl:grid-rows-3-equal">
         {posts.map((post) => (
           <BoardCard post={post} key={post.id} />
