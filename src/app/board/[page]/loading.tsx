677c607f8f468ca102e023e0f1b4e45cf170bd00
rlaclghks123 <rlaclghks123@naver.com>
2024-08-18 19:08:26 +0900

    style: Board Skeleton UI 변경

--- src/app/board/[page]/loading.tsx
+++ src/app/board/[page]/loading.tsx
@@ -4,7 +4,7 @@ export default function loading() {
   return (
     <div className="h-screen-without-nav pb-20 pt-5">
       <div className="mb-2 flex h-8 justify-end" />
-      <div className=" h-full overflow-scroll rounded-lg bg-white">
+      <div className=" h-full overflow-scroll rounded-lg">
         <ol className="grid h-full gap-2 p-2 xs:grid-cols-1 xs:gap-4 sm:grid-cols-2 xl:grid-cols-3 xl:grid-rows-3-equal">
           {Array.from({ length: 9 }, (_, idx) => (
             <BoardCardSkeleton key={idx} />
--- src/container/Board/BoardCardSkeleton.tsx
+++ src/container/Board/BoardCardSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function BoardCardSkeleton() {
   return (
-    <li className="w-full flex-col rounded-lg p-2 xs:h-24 sm:h-32 xl:h-40">
+    <li className="w-full flex-col rounded-lg bg-white p-2 xs:h-24 sm:h-32 xl:h-40">
       <div className="flex h-full items-center ">
         <div className="h-full animate-pulse rounded-xl bg-gray-200 xs:min-w-16 xs:max-w-16 sm:min-w-20 sm:max-w-20 xl:min-w-24 xl:max-w-24" />
         <div className="ml-2 flex h-full w-full flex-col justify-evenly truncate text-xs">
