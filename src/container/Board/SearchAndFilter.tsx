2ece1bc044d86dab651037e0d448b2dfe3955d19
rlaclghks123 <rlaclghks123@naver.com>
2024-08-20 16:17:32 +0900

    refactor: SearchAndFilter 컴포넌트 내부에서 Search 분리

--- src/container/Board/SearchAndFilter.tsx
+++ src/container/Board/SearchAndFilter.tsx
@@ -13,7 +13,7 @@ interface FormData {
   searchBar: string;
 }
 
-export default function SearchAndFilter() {
+function Search() {
   const {
     register,
     reset,
@@ -30,39 +30,44 @@ export default function SearchAndFilter() {
     setValue('searchBar', '');
     router.push(`/board?search=${query}`);
   };
+  return (
+    <form
+      className="relative flex h-full items-center justify-center xs:w-[80%] sm:w-1/2 xl:w-1/3"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Button
+        type="submit"
+        text={<Find className="h-4 w-4 hover:fill-gray-300" />}
+        className="absolute left-2"
+      />
+      <input
+        className="h-[80%] w-full rounded-lg px-8"
+        {...register('searchBar', {
+          required: '내용은 필수 입력 항목입니다',
+        })}
+      />
+      {errors?.searchBar && <ToastUI message={errors.searchBar?.message ?? ''} />}
 
+      <Button
+        type="button"
+        text={<Refresh className="h-4 w-4 hover:fill-gray-300" />}
+        className="absolute right-2"
+        onClick={() => {
+          reset({
+            searchBar: '',
+          });
+          router.push('/board');
+        }}
+      />
+    </form>
+  );
+}
+
+export default function SearchAndFilter() {
   return (
     <div className="w-full0 mt-10 flex h-8 items-center rounded-md px-10">
       <div className="xs:w-[10%] sm:w-1/4 xl:w-1/3" />
-      <form
-        className="relative flex h-full items-center justify-center xs:w-[80%] sm:w-1/2 xl:w-1/3"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <Button
-          type="submit"
-          text={<Find className="h-4 w-4 hover:fill-gray-300" />}
-          className="absolute left-2"
-        />
-        <input
-          className="h-[80%] w-full rounded-lg px-8"
-          {...register('searchBar', {
-            required: '내용은 필수 입력 항목입니다',
-          })}
-        />
-        {errors?.searchBar && <ToastUI message={errors.searchBar?.message ?? ''} />}
-
-        <Button
-          type="button"
-          text={<Refresh className="h-4 w-4 hover:fill-gray-300" />}
-          className="absolute right-2"
-          onClick={() => {
-            reset({
-              searchBar: '',
-            });
-            router.push('/board');
-          }}
-        />
-      </form>
+      <Search />
       <div className="flex w-1/4 justify-end xl:w-1/3" />
     </div>
   );
