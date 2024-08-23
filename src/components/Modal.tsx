d21ebc5c02ff3d2050676050d79bf02f4a196727
rlaclghks123 <rlaclghks123@naver.com>
2024-08-23 19:12:56 +0900

    style: Modal 배경 밝기 낮춤

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,8 +20,8 @@ export default function Modal({ open, children }: Props) {
 
   return createPortal(
     <>
-      <div className="fixed left-0 top-0 z-10 h-screen w-screen bg-black opacity-90" />
-      <div className="xs:w-[80%] xs:left-[10%] absolute top-[15%] z-20 h-[70%] rounded-lg bg-white p-5 sm:left-[15%] sm:w-[70%] xl:left-1/3 xl:w-1/3">
+      <div className="fixed left-0 top-0 z-10 h-screen w-screen bg-black opacity-80" />
+      <div className="absolute top-[15%] z-20 h-[70%] rounded-lg bg-white p-5 xs:left-[10%] xs:w-[80%] sm:left-[15%] sm:w-[70%] xl:left-1/3 xl:w-1/3">
         <div className="h-full w-full">{children}</div>
       </div>
     </>,
